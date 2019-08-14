7612f0215529244aa82ec3f2849e5c70d38f77d9
xuhuan <yi@yi.local>
2019-08-14 15:31:32 +0800

    启用后台定位，添加Notification。

--- src/android/LocationPlugin.java
+++ src/android/LocationPlugin.java
@@ -1,8 +1,12 @@
 package com.kit.cordova.AMapLocation;
 
 import android.Manifest;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -68,11 +72,54 @@ public class LocationPlugin extends CordovaPlugin {
         }
         if ("stoplocation".equals(action)) {
             if (locationClient != null) {
+                locationClient.disableBackgroundLocation(true);
                 locationClient.stopLocation();
             }
             return true;
         }
         return false;
+    }
+
+    private static final String NOTIFICATION_CHANNEL_NAME = "BackgroundLocation";
+    private NotificationManager notificationManager = null;
+    boolean isCreateChannel = false;
+//    @SuppressLint("NewApi")
+    private Notification buildNotification() {
+
+        Notification.Builder builder = null;
+        Notification notification = null;
+        if(android.os.Build.VERSION.SDK_INT >= 26) {
+            //Android O上对Notification进行了修改，如果设置的targetSDKVersion>=26建议使用此种方式创建通知栏
+            if (null == notificationManager) {
+                notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            }
+            String channelId = "外江灌区";
+            if(!isCreateChannel) {
+                NotificationChannel notificationChannel = new NotificationChannel(channelId,
+                        NOTIFICATION_CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT);
+                notificationChannel.enableLights(true);//是否在桌面icon右上角展示小圆点
+                notificationChannel.setLightColor(Color.BLUE); //小圆点颜色
+                notificationChannel.setShowBadge(true); //是否在久按桌面图标时显示此渠道的通知
+                notificationManager.createNotificationChannel(notificationChannel);
+                isCreateChannel = true;
+            }
+            builder = new Notification.Builder(context, channelId);
+        } else {
+            builder = new Notification.Builder(context);
+        }
+        builder.setContentTitle("外江灌区")
+                .setContentText("正在后台运行")
+                .setWhen(System.currentTimeMillis());
+        if(android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O){
+            builder.setChannelId("外江灌区");
+        }
+
+        if (android.os.Build.VERSION.SDK_INT >= 16) {
+            notification = builder.build();
+        } else {
+            return builder.getNotification();
+        }
+        return notification;
     }
 
     private void getLocation() {
@@ -84,6 +131,7 @@ public class LocationPlugin extends CordovaPlugin {
         locationClient.setLocationOption(locationOption); // 设置定位参数
         // 设置场景模式后最好调用一次stop，再调用start以保证场景模式生效
         locationClient.stopLocation();
+        locationClient.enableBackgroundLocation(2001, buildNotification());
         locationClient.startLocation(); // 启动定位
         PluginResult r = new PluginResult(PluginResult.Status.NO_RESULT);
         r.setKeepCallback(true);
